9f544b148c4ee64a530f21d3c5fff6882a721223
ArdaKe26 <arda.kurtulus26@gmail.com>
2026-02-14 15:14:10 +0300

    created the first classes

--- Constants.java
+++ Constants.java
@@ -1,3 +1,6 @@
 public class Constants{
-
+    public class BoardConstants {
+        public static final int NUMBER_OF_ROWS = 6;
+        public static final int NUMBER_OF_COLUMNS = 7;         
+    }
 }
--- Container.java
+++ Container.java
@@ -1,3 +1,5 @@
 public class Container {
-    
+    public class BoardContainer {
+        public static Cell cell = new Cell(Constants.BoardConstants.NUMBER_OF_ROWS, Constants.BoardConstants.NUMBER_OF_COLUMNS);
+    }
 }
